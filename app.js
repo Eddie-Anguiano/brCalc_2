6079105a459c023669abdbcbc7caafa3dd13d7a7
Eddie Anguiano <eddiesanguiano@gmail.com>
2019-07-31 15:37:31 -0700

    switch modules to arrow functions

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
 
-const uiController = (function () {
-
+const uiController = (() => {
   const DOMstrings = {
     totalSalesInput: '.totalSales__input',
     ccTipsInput: '.CcTips__input',
     cashTipsInput: '.cashTips__input',
     mdrTipsInput: '.MdrTips__input',
-
+    nameInput: '.name__input',
+    numberInput: '.number__input',
+    hoursInput: '.hours_input',
+    groupInput: '.group_input',
+    addItem: '.add_item',
   };
+})();
 
+const dataController = (() => {})();
 
-}());
-
-const dataController = (function () {}());
-
-const controller = (function (uiCtrl, dataCtrl) {}(uiController, dataController));
+const controller = ((uiCtrl, dataCtrl) => {})(uiController, dataController);
