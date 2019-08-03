c4abab7bbe4b3b2f684d8a6927f78c6edf7ea207
Eddie Anguiano <eddiesanguiano@gmail.com>
2019-08-02 20:51:03 -0700

    data structure ready to be implemented

--- app.js
+++ app.js
@@ -3,18 +3,57 @@
 
 const uiController = (() => {
   const DOMstrings = {
-    totalSalesInput: '.totalSales__input',
-    ccTipsInput: '.CcTips__input',
-    cashTipsInput: '.cashTips__input',
-    mdrTipsInput: '.MdrTips__input',
-    nameInput: '.name__input',
-    numberInput: '.number__input',
-    hoursInput: '.hours_input',
-    groupInput: '.group_input',
-    addItem: '.add_item',
+    totalSalesInput: '#totalSales__input',
+    ccTipsInput: '#CcTips__input',
+    cashTipsInput: '#cashTips__input',
+    mdrTipsInput: '#MdrTips__input',
+    nameInput: '#name__input',
+    numberInput: '#number__input',
+    hoursInput: '#hours__input',
+    minutesInput: '#minutes__input',
+    addItem: '#add_item',
+  };
+
+  return {
+    getDOMstrings() {
+      return DOMstrings;
+    },
+    getInputs() {
+      return {
+        totalSalesValue: parseFloat(document.querySelector(DOMstrings.totalSalesInput).value),
+        ccTipsValue: parseFloat(document.querySelector(DOMstrings.ccTipsInput).value),
+        cashTipsValue: parseFloat(document.querySelector(DOMstrings.cashTipsInput).value),
+        mdrTipsValue: parseFloat(document.querySelector(DOMstrings.mdrTipsInput).value),
+        nameValue: document.querySelector(DOMstrings.nameInput).value,
+        numberValue: parseFloat(document.querySelector(DOMstrings.numberInput).value),
+        hoursValue: parseFloat(document.querySelector(DOMstrings.hoursInput).value),
+        minutesValue: parseFloat(document.querySelector(DOMstrings.minutesInput).value),
+      };
+    },
   };
 })();
 
 const dataController = (() => {})();
 
-const controller = ((uiCtrl, dataCtrl) => {})(uiController, dataController);
+const controller = ((uiCtrl, dataCtrl) => {
+  function setUpEventListeners() {
+    const DOM = uiCtrl.getDOMstrings();
+    document.querySelector(DOM.addItem).addEventListener('click', () => {
+      // get all inputs
+      const allValues = uiCtrl.getInputs();
+      console.log(allValues);
+      // create data structure
+      // calculate
+      // display group block(s)
+    });
+  }
+
+  return {
+    init() {
+      console.log('START');
+      setUpEventListeners();
+    },
+  };
+})(uiController, dataController);
+
+controller.init();
