f8e4b9735bb3d551cc45b8bc6e812d3255519197
Eddie Anguiano <eddiesanguiano@gmail.com>
2019-08-04 20:13:31 -0700

    data structure implemented, still needs testing

--- app.js
+++ app.js
@@ -33,7 +33,117 @@ const uiController = (() => {
   };
 })();
 
-const dataController = (() => {})();
+const dataController = (() => {
+  let totalHours;
+  const data = {
+    totals: {},
+    groups: [],
+  };
+
+  class Total {
+    constructor(sales, cashTips, ccTips, mdrTips) {
+      this.sales = parseFloat(sales);
+      this.cashTips = parseFloat(cashTips);
+      this.ccTips = parseFloat(ccTips);
+      this.mdrTips = parseFloat(mdrTips);
+    }
+
+    totalTips() {
+      return this.cashTips + this.ccTips;
+    }
+
+    salesPerHour() {
+      return this.sales / totalHours;
+    }
+
+    cashTipsPerHour() {
+      return this.cashTips / totalHours;
+    }
+
+    ccTipsPerHour() {
+      return this.cashTips / totalHours;
+    }
+
+    busTipOutPerHour() {
+      const busTipOutTotal = this.mdrTips * 0.1;
+      return busTipOutTotal / totalHours;
+    }
+
+    barTipOutPerHour() {
+      const barTipOutTotal = this.totalTips() * 0.5;
+      return barTipOutTotal / totalHours;
+    }
+
+    expoTipOutPerHour() {
+      const expoTipOutTotal = this.totalTips() * 0.5;
+      return expoTipOutTotal / totalHours;
+    }
+  }
+
+  class Group {
+    constructor(name, members, hoursPer, minutes) {
+      this.name = name;
+      this.members = parseInt(members, 10);
+      this.hoursPer = parseInt(hoursPer, 10);
+      this.minutes = parseInt(minutes, 10);
+    }
+
+    decimalTimePerMember() {
+      const decimalMinutes = this.minutes / 60;
+      return this.hoursPer + decimalMinutes;
+    }
+
+    groupHours() {
+      return this.decimalTimePerMember() * this.members;
+    }
+
+    salesPerMember() {
+      return this.decimalTimePerMember() * Total.salesPerHour();
+    }
+
+    cashTipsPerMember() {
+      return this.decimalTimePerMember() * Total.cashTipsPerHour();
+    }
+
+    ccTipsPerMember() {
+      return this.decimalTimePerMember() * Total.ccTipsPerHour();
+    }
+
+    busTipOutPerMember() {
+      return data.totals.busTipOutPerHour() * this.decimalTimePerMember();
+    }
+
+    barTipOutPerMember() {
+      return data.totals.barTipOutPerHour() * this.decimalTimePerMember();
+    }
+
+    expoTipOutPerMember() {
+      return data.totals.expoTipOutPerHour() * this.decimalTimePerMember();
+    }
+
+    totalTipOutPerMember() {
+      return this.busTipOutPerMember() + this.barTipOutPerMember() + this.expoTipOutPerMember();
+    }
+  }
+
+  return {
+    updateTotalHours() {
+      totalHours = data.groups.reduce((accumulator, item) => accumulator + item.groupHours(), 0);
+    },
+    setGroup(name, members, hours, minutes) {
+      const group = new Group(name, members, hours, minutes);
+      data.groups.push(group);
+    },
+    setTotals(sales, cashTips, ccTips, mdrTips) {
+      const totalsObj = new Total(sales, cashTips, ccTips, mdrTips);
+      data.totals = totalsObj;
+    },
+    testControl() {
+      // console.log(data.totals.busTipOutPerMember());
+      console.log(data.groups[0].barTipOutPerMember());
+    },
+  };
+})();
 
 const controller = ((uiCtrl, dataCtrl) => {
   function setUpEventListeners() {
@@ -57,3 +167,9 @@ const controller = ((uiCtrl, dataCtrl) => {
 })(uiController, dataController);
 
 controller.init();
+dataController.setTotals(1000, 100, 100, 0);
+dataController.setGroup("4o'clockers", 2, 2, 0);
+// dataController.setGroup("5o'clockers", 2, 5, 0);
+
+dataController.updateTotalHours();
+dataController.testControl();
